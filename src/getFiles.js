65b29cf930fe9070aae6a552f6852591f4c5e6a8
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2019-01-27 19:18:01 -0500

    Update getFiles.js

--- src/getFiles.js
+++ src/getFiles.js
@@ -1,4 +1,25 @@
+var md5ext_arr = [];
+
+function recursive_md5extSearch(object) {
+var x;
+  for(x in projectJSON) {
+  if(x="md5ext") {
+  md5ext_arr.push(projectJSON.x);
+  } else if (typeof(x)=="object" || typeof(x)=="array") {
+  recursive_md5extSearch(x);
+  }
+  }
+}
+
 function getFiles() {
-var projectJSON = read("project.json");
+var projectJSON = JSON.parse(read("project.json"));
 // read all "md5ext" attributes into an array
+  var x;
+  for(x in projectJSON) {
+  if(x="md5ext") {
+  md5ext_arr.push(projectJSON.x);
+  } else {
+  recursive_md5extSearch(x);
+  }
+  }
 }
