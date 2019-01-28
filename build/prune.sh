1d6f6e473ea919e99b81a90b1aa4e9b31d480ea2
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2019-01-27 20:08:36 -0500

    Update prune.sh

--- build/prune.sh
+++ build/prune.sh
@@ -1,1 +1,25 @@
-
+cd lib
+rm exec.js
+rm downloadAsset.js
+cd ..
+cd src
+rm download.js
+rm getFiles.js
+cd ..
+cd build
+rm start.sh
+rm prune.sh
+cd ..
+rmdir lib
+rmdir src
+rmdir build
+rm LICENSE.txt
+rm build.py
+rm README.md
+rm curltotestfile.sh
+rm manifest.json
+rm nodeShellExec.sh
+rm testfile.txt
+rm lib.js
+rm src.js
+rm build.js
